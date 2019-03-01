3a4358122e6d6a7430957803c112849bc11efa4e
Thanh Vu <thanh@ahiho.com>
2019-03-01 14:14:33 +0700

    add some simple code

--- main.go
+++ main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
+	"time"
+
+	"github.com/jvthanh/u/pkg/healthz"
+	"github.com/jvthanh/u/pkg/readyz"
+	"github.com/jvthanh/u/pkg/version"
+)
 
 func main() {
-	fmt.Println("Welcome to u microservice!")
+	log.Printf(
+		"Starting the service...\ncommit: %s, build time: %s, release: %s",
+		version.Commit, version.BuildTime, version.Release,
+	)
+
+	router := http.NewServeMux()
+
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+	go func() {
+		log.Printf("Readyz probe is negative by default...")
+		time.Sleep(10 * time.Second)
+		isReady.Store(true)
+		log.Printf("Readyz probe is positive.")
+	}()
+
+	router.HandleFunc("/healthz", healthz.Handler)
+	router.HandleFunc("/readyz", readyz.Handler(isReady))
+	router.HandleFunc("/", version.Handler)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: accessControl(router),
+	}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	log.Print("uService service is ready to listen and serve.")
+
+	killSignal := <-interrupt
+	switch killSignal {
+	case os.Kill:
+		log.Print("Got SIGKILL...")
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Got SIGTERM...")
+	}
+
+	log.Print("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("Done")
+
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
---
+++ pkg/readyz/readyz.go
@@ -0,0 +1,17 @@
+package readyz
+
+import (
+	"net/http"
+	"sync/atomic"
+)
+
+// Handler readyz is a readiness probe.
+func Handler(isReady *atomic.Value) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		if isReady == nil || !isReady.Load().(bool) {
+			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
